30d17784126adef055fb1fd19f9ba6cddff75fae
fabiodagostino27 <fabiodago27@gmail.com>
2025-09-03 18:15:40 +0200

    Refactor game initialization and implement complete fighting phase logic

--- js/index.js
+++ js/index.js
@@ -1,66 +1,78 @@
 import { fighters, weapons, robot } from "./data.js";
 
-console.log(`Ecco i partecipanti al torneo Boolkaichi:`);
-console.log(fighters);
+const startButton = document.getElementById("start");
 
-// Fase 1 - 🔥 Scelta dell'Arma: ogni combattente sceglierà casualmente un'arma dalla relativa lista. Una volta scelta, un'arma non sarà più disponibile per i successivi combattenti.
-console.log("I combattenti scelgono un'arma!");
+startButton.addEventListener("click", () => {
+    startGame(fighters, weapons, robot)
+});
 
-function weaponsChoice(fighters, weapons) {
-    fighters.forEach(fighter => {
-       const randomIndex = Math.floor(Math.random() * weapons.length); 
-       const randomWeapon = weapons[randomIndex];
-       fighter.weapon = randomWeapon.name;
-       fighter.power += randomWeapon.power;
-       weapons.splice(randomIndex, 1);
-       console.log(`${fighter.name} sceglie ${fighter.weapon} portando il suo potere a ${fighter.power}!`)
-    });
-}
+function startGame(fighters, weapons, robot) {
+    const tempFighters = JSON.parse(JSON.stringify(fighters));
+    const tempWeapons = JSON.parse(JSON.stringify(weapons));
 
-weaponsChoice(fighters, weapons);
+    console.log(`Ecco i partecipanti al torneo Boolkaichi:`);
+    console.log(tempFighters);
 
-// Fase 2 - 💪 Allenamento: ogni combattente si sottoporrà ad un allenamento che incrementerà (o forse no) la sua potenza, moltiplicandola per un numero casuale tra 1 e 100.
-console.log("I combattenti si allenano!");
+    // Fase 1 - 🔥 Scelta dell'Arma: ogni combattente sceglierà casualmente un'arma dalla relativa lista. Una volta scelta, un'arma non sarà più disponibile per i successivi combattenti.
 
-function training(fighters) {
-    fighters.forEach(fighter => {
-        fighter.power *= Math.floor(Math.random() * 101);
-        console.log(`La potenza di ${fighter.name} arriva a ${fighter.power}!`)
-    })
-}
-
-training(fighters);
-
-// Fase 3 -  🎯 Qualificazione: escludiamo dal torneo chi, dopo l'allenamento non è riuscito a raggiungere una potenza di almeno 2000.
-console.log("I combattenti che non hanno raggiunto una potenza di 2000 sono stati esclusi, ecco quindi la lista finale: ")
-
-let qualifiedFighters = [];
-function filterWeakFighters(fighters) {
-    qualifiedFighters = fighters.filter(fighter => fighter.power > 2000);
-}
-filterWeakFighters(fighters);
-
-console.log(qualifiedFighters);
-
-// Fase 4 - ⚔️ Combattimento: i combattimenti si svolgeranno tra un partecipante e il successivo dell'elenco, assicurandosi che ognuno combatta una sola volta. 
-console.log("I combattenti si sfidano!");
-const winners = [];
-function fightingPhase(qualifiedFighters) {
-    if (qualifiedFighters.length % 2 != 0) qualifiedFighters.push(robot);
-    
-    for (let i = 0; i < qualifiedFighters.length - 1; i = i + 2) {
-        console.log(`${qualifiedFighters[i].name} e ${qualifiedFighters[i + 1].name} si sfidano!`)
-        if ((qualifiedFighters[i].power > qualifiedFighters[i + 1].power) || qualifiedFighters[i].power === qualifiedFighters[i + 1].power ) {
-            console.log(`Vince ${qualifiedFighters[i].name}!`);
-            winners.push(qualifiedFighters[i]);
-        } else if (qualifiedFighters[i].power < qualifiedFighters[i + 1].power) {
-            console.log(`Vince ${qualifiedFighters[i + 1].name}!`);
-            winners.push(qualifiedFighters[i + 1]);
-        }
+    function weaponsChoice(tempFighters, tempWeapons) {
+        console.log("I combattenti scelgono un'arma!");
+        tempFighters.forEach(fighter => {
+            const randomIndex = Math.floor(Math.random() * tempWeapons.length);
+            const randomWeapon = tempWeapons[randomIndex];
+            fighter.weapon = randomWeapon.name;
+            fighter.power += randomWeapon.power;
+            tempWeapons.splice(randomIndex, 1);
+            console.log(`${fighter.name} sceglie ${fighter.weapon} portando il suo potere a ${fighter.power}!`)
+        });
     }
 
-    winners.sort((a, b) => b.power - a.power)
-    return winners.filter((winner, index) => index < 3);
+    weaponsChoice(tempFighters, tempWeapons);
+
+    // Fase 2 - 💪 Allenamento: ogni combattente si sottoporrà ad un allenamento che incrementerà (o forse no) la sua potenza, moltiplicandola per un numero casuale tra 1 e 100.
+
+    function training(tempFighters) {
+        console.log("I combattenti si allenano!");
+        tempFighters.forEach(fighter => {
+            fighter.power *= Math.floor(Math.random() * 101);
+            console.log(`La potenza di ${fighter.name} arriva a ${fighter.power}!`)
+        })
+    }
+
+    training(tempFighters);
+
+    // Fase 3 -  🎯 Qualificazione: escludiamo dal torneo chi, dopo l'allenamento non è riuscito a raggiungere una potenza di almeno 2000.
+    console.log("I combattenti che non hanno raggiunto una potenza di 2000 sono stati esclusi, ecco quindi la lista finale: ")
+
+    let qualifiedFighters = [];
+    function filterWeakFighters(fighters) {
+        qualifiedFighters = tempFighters.filter(fighter => fighter.power > 2000);
+    }
+    filterWeakFighters(tempFighters);
+
+    console.log(qualifiedFighters);
+
+    // Fase 4 - ⚔️ Combattimento: i combattimenti si svolgeranno tra un partecipante e il successivo dell'elenco, assicurandosi che ognuno combatta una sola volta. 
+    console.log("I combattenti si sfidano!");
+    const winners = [];
+    function fightingPhase(qualifiedFighters) {
+        if (qualifiedFighters.length % 2 != 0) qualifiedFighters.push(robot);
+
+        for (let i = 0; i < qualifiedFighters.length - 1; i = i + 2) {
+            console.log(`${qualifiedFighters[i].name} e ${qualifiedFighters[i + 1].name} si sfidano!`)
+            if ((qualifiedFighters[i].power > qualifiedFighters[i + 1].power) || qualifiedFighters[i].power === qualifiedFighters[i + 1].power) {
+                console.log(`Vince ${qualifiedFighters[i].name}!`);
+                winners.push(qualifiedFighters[i]);
+            } else if (qualifiedFighters[i].power < qualifiedFighters[i + 1].power) {
+                console.log(`Vince ${qualifiedFighters[i + 1].name}!`);
+                winners.push(qualifiedFighters[i + 1]);
+            }
+        }
+
+        winners.sort((a, b) => b.power - a.power)
+        return winners.filter((winner, index) => index < 3);
+    }
+
+    console.log(fightingPhase(qualifiedFighters))
 }
 
-console.log(fightingPhase(qualifiedFighters))
